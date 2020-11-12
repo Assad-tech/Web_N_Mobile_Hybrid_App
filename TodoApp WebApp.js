700ae2e5825bb10a48538e439bf61ccb58fb415f
Assad-tech <assadphl@gmail.com>
2020-11-12 12:17:54 +0500

    ToDo App web app Assignment

--- TodoApp WebApp.js
+++ TodoApp WebApp.js
@@ -1,33 +1,49 @@
 // TODO App
+// Geting Back data from Firebase.
+
+firebase.database().ref("Do This").on('child_added',function(data){
+    console.log(data.val())
+})
+
 function addTodo(){
     var todo_item = document.getElementById("todo-task")
 
-    // Li Items and Text inserted.
-    var li = document.createElement("li")
-    var liText = document.createTextNode(todo_item.value)
-    li.setAttribute("class","LItems")
-    li.appendChild(liText)
+    // Now Working On FireBase Database to save data onLne. After the completion of WebApp in Functiins.
 
-    // Creating Edit Btn in Li
-    var editBtn = document.createElement("button")
-    var editText = document.createTextNode("Edit")
-    editBtn.setAttribute("class","btnEdit")
-    editBtn.setAttribute("onclick","editItem(this)")
-    editBtn.appendChild(editText)
+    var var_Database = firebase.database().ref("Do This")
+    var key = var_Database.push().key;
+    var todo_Obj ={
+        value: todo_item.value,
+        key: key
+    }
+    var_Database.child(key).set(todo_Obj)
 
-    // Creating Delete Btn in Li
-    var delBtn = document.createElement("button")
-    var delText = document.createTextNode("Delete")
-    //setAttribute = takes two items, 1 Attribute Name. 2 Attribute Value. 
-    delBtn.setAttribute("class","btnDel")
-    delBtn.setAttribute("onclick","deletItem(this)")
-    delBtn.appendChild(delText)
+    // // Li Items and Text inserted.
+    // var li = document.createElement("li")
+    // var liText = document.createTextNode(todo_item.value)
+    // li.setAttribute("class","LItems")
+    // li.appendChild(liText)
 
-    li.appendChild(editBtn)
-    li.appendChild(delBtn)
-    todo_list.appendChild(li)
+    // // Creating Edit Btn in Li
+    // var editBtn = document.createElement("button")
+    // var editText = document.createTextNode("Edit")
+    // editBtn.setAttribute("class","btnEdit")
+    // editBtn.setAttribute("onclick","editItem(this)")
+    // editBtn.appendChild(editText)
+
+    // // Creating Delete Btn in Li
+    // var delBtn = document.createElement("button")
+    // var delText = document.createTextNode("Delete")
+    // //setAttribute = takes two items, 1 Attribute Name. 2 Attribute Value. 
+    // delBtn.setAttribute("class","btnDel")
+    // delBtn.setAttribute("onclick","deletItem(this)")
+    // delBtn.appendChild(delText)
+
+    // li.appendChild(editBtn)
+    // li.appendChild(delBtn)
+    // todo_list.appendChild(li)
     
-    todo_item.value = ""
+    // todo_item.value = ""
 }
 
 // The LIST of all Items.
